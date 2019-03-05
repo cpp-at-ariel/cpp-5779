dd8256b2d61a20b76c5d8b143011b569dcf875a5
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-03-05 17:00:03 +0200

    Week 2 tuesday updates

--- 02-classes-constructors-destructors/3-static/points.index.cpp
+++ 02-classes-constructors-destructors/3-static/points.index.cpp
@@ -1,5 +1,6 @@
 /**
- * Demonstrates static members
+ * Demonstrates static members.
+ * Creates a class "point" with static members for giving a unique index to each object.
  */
 
 #include <iostream>
