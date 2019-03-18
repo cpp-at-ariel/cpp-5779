d3d5c20738b0b60089664eb562abc1cda7429049
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-03-18 12:27:01 +0200

    Add line numbers to the badkan

--- 02-classes-constructors-destructors/homework/badkan.hpp
+++ 02-classes-constructors-destructors/homework/badkan.hpp
@@ -19,12 +19,10 @@
 
 #pragma once
 
-#define CHECK_OK(actual) check_ok([&](){actual;})
-#define CHECK_THROWS(actual) check_throws([&](){actual;})
-#define CHECK_EQUAL(actual,expected) check_equal([&](){return actual;}, expected)
-#define CHECK_OUTPUT(actual,expected) check_equal([&](){ostringstream ostr; ostr<<"'"<<actual<<"'"; return ostr.str();}, "'"+string(expected)+"'")
-
-// #define CHECK_OUTPUT(actual,expected) check_output([&](){return actual;}, expected)
+#define CHECK_OK(actual) check_ok([&](){actual;}, __FILE__+string(":")+to_string(__LINE__))
+#define CHECK_THROWS(actual) check_throws([&](){actual;}, __FILE__+string(":")+to_string(__LINE__))
+#define CHECK_EQUAL(actual,expected) check_equal([&](){return actual;}, expected, __FILE__+string(":")+to_string(__LINE__))
+#define CHECK_OUTPUT(actual,expected) check_equal([&](){ostringstream ostr; ostr<<"'"<<actual<<"'"; return ostr.str();}, "'"+string(expected)+"'", __FILE__+string(":")+to_string(__LINE__))
 #define CHECK_INPUT(actual,expected) check_input(actual, expected)
 
 #include <string>
@@ -34,7 +32,7 @@
 #include <ctime>
 #include <csignal>
 #include <csetjmp>
-using std::string, std::ostream, std::endl, std::cerr, std::exception, std::ostringstream,  std::istringstream;
+using std::string, std::ostream, std::endl, std::cerr, std::exception, std::ostringstream,  std::istringstream, std::to_string;
 
 
 namespace badkan {
@@ -76,44 +74,44 @@ public:
   }
 
   TestCase& print()  {
-    print(this->output, /*show_grade=*/true);
+    print(output, /*show_grade=*/true);
     return *this;
   }
   
   TestCase& print_timeout()  {
-    this->output << "Your program timed out! (caught signal TERM)" << endl;
-    print(this->output, /*show_grade=*/false);
+    output << "Your program timed out! (caught signal TERM)" << endl;
+    print(output, /*show_grade=*/false);
     return *this;
   }
 
 
 
   /** Checks that the given function is OK, i.e., does not throw an exception */
-  template<typename TFUNC> TestCase& check_ok(const TFUNC actual_func) { 
+  template<typename TFUNC> TestCase& check_ok(const TFUNC actual_func, const string& context) { 
     try {
-      get_actual_value<TFUNC,void>(actual_func);
+      get_actual_value<TFUNC,void>(actual_func, context);
       passed++;
     } catch(...) {}
     return *this;
   }
   
   /** Checks that the given function throws an exception */
-  template<typename TFUNC> TestCase& check_throws(const TFUNC actual_func) { 
+  template<typename TFUNC> TestCase& check_throws(const TFUNC actual_func, const string& context) { 
     try {
       actual_func();
       failed++;
-      output << title() << "There should be an exception!" << endl;
+      output << context << ", "  << title() << "There should be an exception!" << endl;
     } catch(...) {
       passed++;
     }
     return *this;
   }
 
-  template<typename TFUNC, typename TVAL> TestCase& check_equal(const TFUNC actual_func, const TVAL& expected_value) {
+  template<typename TFUNC, typename TVAL> TestCase& check_equal(const TFUNC actual_func, const TVAL& expected_value, const string& context) {
     try {
-      TVAL actual_value = get_actual_value<TFUNC,TVAL>(actual_func);
+      TVAL actual_value = get_actual_value<TFUNC,TVAL>(actual_func, context);
       if (incorrect(actual_value==expected_value))
-        output << title() << "the result was " << actual_value << " but it should equal " << expected_value << "!" << endl;
+        output << context << ", " << title() << "the result was " << actual_value << " but it should equal " << expected_value << "!" << endl;
     } catch(...) {}
     return *this;
   }
@@ -124,20 +122,20 @@ private:
     return name + " test #" + std::to_string(total()) + ": ";
   }
   
-  template<typename TFUNC, typename TVAL> TVAL get_actual_value(TFUNC actual_func) {
+  template<typename TFUNC, typename TVAL> TVAL get_actual_value(TFUNC actual_func, const string& context) {
     try {
       return actual_func();
     } catch (const string& message) {
       failed++;
-      output << title() << "There was an exception: " << message << endl;
+      output << context << ", "  << title() << "There was an exception: " << message << endl;
       throw;
     } catch (const exception& ex) {
       failed++;
-      output << title() << "There was an exception: " << ex.what() << endl;
+      output << context << ", "  << title() << "There was an exception: " << ex.what() << endl;
       throw;
     } catch (...) {
       failed++;
-      output << title() << "There was an exception." << endl;
+      output << context << ", "  << title() << "There was an exception." << endl;
       throw;
     }
   }
