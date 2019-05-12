f215f0d341ba1bd9b3c78dd47db30755cf4ffe6a
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-05-12 16:07:35 +0300

    add to_string to demo

--- 08-templates-iterators/homework/Demo.cpp
+++ 08-templates-iterators/homework/Demo.cpp
@@ -7,6 +7,7 @@
 
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 #include "range.hpp"
@@ -16,10 +17,19 @@ using namespace std;
 #include "powerset.hpp"
 using namespace itertools;
 
+template<typename Iterable>
+string iterable_to_string(const Iterable& iterable) {
+	ostringstream ostr;
+	for (decltype(*iterable.begin()) i: iterable)
+		ostr << i << ",";
+	return ostr.str();
+}
+
 int main() {
 	cout << endl << endl << "Range of ints: " << endl;
 	for (int i: range(5,9))
 		cout << i;    // 5678
+	cout << endl;
 	cout << endl << endl << "Range of doubles: " << endl;
 	for (double i: range(5.1,9.1))
 		cout << i << " ";    // 5.1 6.1 7.1 8.1
@@ -57,6 +67,8 @@ int main() {
 	cout << endl << endl << "Power-set of chain " << endl;
 	for (auto subset: powerset(chain(range('a','c'),range('x','z'))))
 		cout << subset;  // {}{a}{b}{a,b}{x}{a,x}{b,x}{a,b,x}{y}{a,y}{b,y}{a,b,y}{x,y}{a,x,y}{b,x,y}{a,b,x,y}
+	cout << endl;
+	cout << iterable_to_string(powerset(chain(range('a','c'),range('x','z')))) << endl;
 	return 0;
 }
 
